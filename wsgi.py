1d287a47ee6f97fff6522e5e6be384ebe80da634
Xavier Revelo <x.revelo007@gmail.com>
2026-09-06 17:08:07 -0500

    Expose app variable in wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,7 @@
-import sys
-import os
-
-# Agrega la ruta actual al sistema para asegurar que reconozca los módulos
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
 from app import *
 
-# Si tu archivo principal dentro de app expone la variable app:
+# Si tu aplicación Flask se inicializa en app/__init__.py o similar:
 try:
-    from app.app import app
+    from app import app
 except ImportError:
-    try:
-        from app.main import app
-    except ImportError:
-        pass
+    from app.main import app
